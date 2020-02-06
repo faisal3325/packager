51a117d7d0621f54ca586131314a7f2dc4a841d0
faisal.khan <faisal.khan@electroroute.com>
2020-02-06 12:48:20 +0000

    minor changes

--- main.py
+++ main.py
@@ -6,34 +6,52 @@ import conf as conf
 
 
 def main(file_path):
+    # move to file_path
     os.chdir(file_path)
-    curr_dir = os.listdir()
-    print(curr_dir)
+    files = os.listdir()
 
-    if conf.modules in curr_dir:
-        os.chdir(conf.modules)
-        modules_dirs = os.listdir()
+    # prepare layers
+    prepareLayers(files)
 
-    for dir in modules_dirs:
-        os.chdir(dir)
-        content = filter(filterFiles, os.listdir())
+    # prepare functions
+    # prepareFunctions(files)
 
-        if not os.path.exists(conf.layer_path):
-            os.makedirs(conf.layer_path)
 
-        for file in list(content):
-            if file == 'python':
-                continue
-            print(file)
-            shutil.copyfile(file, f"{os.curdir}/{conf.layer_path}/{file}")
-            print(f"{os.curdir}/{conf.layer_path}/{file}")
-            # shutil.copy(file, f"{os.curdir}/{conf.layer_path}/{file}")
+def prepareLayers(files):
+    # check if module present
+    if conf.module not in files:
+        return
 
-        zipf = zipfile.ZipFile('python.zip', 'w', zipfile.ZIP_DEFLATED)
-        zipdir('python/', zipf)
-        zipf.close()
+    # change to module dir
+    os.chdir(conf.module)
 
-        os.chdir("..")
+    # get all modules
+    modules = list(
+        filter(lambda file: file.startswith('ER_'), os.listdir()))
+    print(modules)
+
+    # process modules
+    processFile(modules, target=conf.layer_path)
+
+    os.chdir("..")
+
+
+def prepareFunctions(files):
+    # check if script present
+    if conf.script not in files:
+        return
+
+    # change to script dir
+    os.chdir(conf.script)
+
+    # get all scripts
+    scripts = os.listdir()
+    print(scripts)
+
+    # process scripts
+    processFile(scripts)
+
+    os.chdir("..")
 
 
 def filterFiles(file):
@@ -41,6 +59,63 @@ def filterFiles(file):
         return True
     else:
         return False
+
+
+def processFile(files, target=None):
+    # loop through each file to process
+    for file in files:
+        if target is None:
+            target = file
+        # move to file
+        os.chdir(file)
+
+        recursivelyGetFiles(file, target)
+
+        zip(target)
+
+        toDelete = target.split('/')[0]
+        if os.path.exists(f'{toDelete}'):
+            shutil.rmtree(f'{toDelete}')
+
+        os.chdir("..")
+
+
+def recursivelyGetFiles(path, target):
+    # get files in path
+    print(path, target)
+    print(os.listdir())
+    files = list(filter(lambda file: filterFiles(file), os.listdir()))
+    print(files)
+    # loop through all and check wether file is dir
+    for file in files:
+        if isDir(file):
+            os.chdir(file)
+            recursivelyGetFiles(file, f"{target}/{file}")
+            os.chdir("..")
+        else:
+            copyFile(file, path, target)
+
+
+def copyFile(file, path, target):
+    # create target path
+    if not os.path.exists(target):
+        os.makedirs(target)
+    shutil.copyfile(file, f"{target}/{file}")
+
+
+def isDir(source):
+    try:
+        os.chdir(source)
+        os.chdir('..')
+        return True
+    except Exception:
+        return False
+
+
+def zip(target):
+    zipf = zipfile.ZipFile('python.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipdir('python/', zipf)
+    zipf.close()
 
 
 def zipdir(path, ziph):
